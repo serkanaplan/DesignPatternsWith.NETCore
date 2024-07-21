6b23d7c63de64c3619a035e4bc68bc347ca237cd
Serkan Kaplan <serkanaplan@hotmail.com>
2024-07-21 03:37:57 +0300

    Base Project Completed

---
+++ BaseProject/Models/AppIdentityDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProject.Models;
+
+public class AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : IdentityDbContext<AppUser>(options)
+{
+}
