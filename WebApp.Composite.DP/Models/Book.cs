5613a6e463ce08b5ae149e3b4a96aa98f02f703c
Serkan Kaplan <serkanaplan@hotmail.com>
2024-07-23 00:52:25 +0300

    Composite Pattern Completed

---
+++ WebApp.Composite.DP/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WebApp.Composite.DP.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Category Category { get; set; }
+
+    public int CategoryId { get; set; }
+}
